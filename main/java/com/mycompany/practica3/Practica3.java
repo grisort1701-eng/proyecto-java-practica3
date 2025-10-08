2b43971202b4fc3676d183f4faaa4cc2beaad0d8
Gris Ortiz <grisort1701@gmail.com>
2025-10-07 19:33:11 -0600

    Subiendo mi proyecto completo

---
+++ main/java/com/mycompany/practica3/Practica3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica3;
+
+/**
+ *
+ * @author ESCOM
+ */
+import java.util.Scanner;
+public class Practica3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!");
+        String nombre= sc.nextLine();
+        System.out.println("PRACTICA 3: TIRO CON ARCO");
+    }
+}
